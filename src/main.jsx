2fc3fee43fd33e825135b43c56ab524dbe965ca0
Pica <>
2025-08-14 11:38:38 -0300

    Prueba con hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+<HashRouter basename={import.meta.env.BASE_URL}>
+  <App />
+</HashRouter>
+
   </React.StrictMode>
 );
